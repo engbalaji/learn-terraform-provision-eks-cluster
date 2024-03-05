1fa1d71975e4cec59faac98b661b005c42f3469c
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-03-05 13:06:01 -0500

    Updated VPC configurations

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ module "vpc" {
 
   
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  #enable_nat_gateway   = true
+  #single_nat_gateway   = true
   enable_dns_hostnames = true
 
 #  public_subnet_tags = {
