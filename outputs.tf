606f645da99752d7890b43aef2a9d703d040f939
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-03-11 14:23:57 -0400

    output file changes

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value       = "eks.cluster_endpoint"
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+  value       = "aws_security_group"
 }
 
 output "region" {
@@ -18,5 +18,5 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+  value       = "eks.cluster_name"
 }
