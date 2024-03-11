e5c12ae3a0ad5f3f6dbc7e2d708f4b2024437fae
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-03-11 17:41:22 -0400

    role changed for ng2

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_eks_node_group" "example_nodes" {
 resource "aws_eks_node_group" "example_nodes_2" {
   cluster_name    = "PEPOC-EKS-Cluster"
   node_group_name = "pe-nodegrp-2"
-  node_role_arn   = "arn:aws:iam::960456129040:role/bmeks2-NodeInstanceRole-1AM6MX3S587QX"
+  node_role_arn   = "arn:aws:iam::960456129040:role/bmeks1-NodeInstanceRole-1AM6MX3S587QX"
   subnet_ids = ["subnet-89eb03a4", "subnet-c496e7e9", "subnet-b64d14ff" ]
   capacity_type = "SPOT"
   
