36c20d05502a38c8b4ad6503bfda80da71547ed6
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-03-18 09:22:26 -0400

    ArgoCD update 3

--- chartsargo.tf
+++ chartsargo.tf
@@ -1,32 +1,7 @@
-# Install traefik helm_chart
 resource "helm_release" "argocd" {
-  namespace        = var.namespace
+  name       = "argocd-helm"
+  namespace  = "argocd"
+ chart      = "argo-cd"
   create_namespace = true
-  name             = var.release_name
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "3.35.4"
-
-  # Helm chart deployment can sometimes take longer than the default 5 minutes
-  timeout = var.timeout_seconds
-
-  # If values file specified by the var.values_file input variable exists then apply the values from this file
-  # else apply the default values from the chart
-  #values = [fileexists("${path.root}/${var.values_file}") == true ? file("${path.root}/${var.values_file}") : ""]
-  values          = [file("argocd.yaml")]
-
-  set_sensitive {
-    name  = "configs.secret.argocdServerAdminPassword"
-    value = var.admin_password == "" ? "" : bcrypt(var.admin_password)
+  repository = "https://argoproj.github.io/argo-helm"
   }
-
-  set {
-    name  = "configs.params.server\\.insecure"
-    value = var.insecure == false ? false : true
-  }
-
-  set {
-    name  = "dex.enabled"
-    value = var.enable_dex == true ? true : false
-  }
-}
